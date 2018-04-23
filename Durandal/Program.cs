fb6f48eb9e700b58207f6c5829589c4f027f147d
Recatek <recatek@hotmail.com>
2018-04-22 23:31:56 -0400

    Baseline connect and command functionality

--- Durandal/Program.cs
+++ Durandal/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Durandal.Services;
 
 namespace Durandal
 {
-    class Program
+  public class Program
+  {
+    public static Task Main(string[] args) => Program.RunAsync(args);
+
+    public static async Task RunAsync(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      DiscordSocketClient client = new DiscordSocketClient();
+      IConfiguration config = Program.BuildConfig();
+
+      IServiceProvider services = Program.ConfigureServices(client, config);
+      services.GetRequiredService<LogService>();
+      await services.GetRequiredService<MessageService>().Initialize(services);
+
+      await client.LoginAsync(TokenType.Bot, config["token"]);
+      await client.StartAsync();
+
+      await Task.Delay(-1);
     }
+
+    private static IServiceProvider ConfigureServices(
+      DiscordSocketClient client,
+      IConfiguration config)
+    {
+      return new ServiceCollection()
+          // Base
+          .AddSingleton(client)
+          .AddSingleton<CommandService>()
+          .AddSingleton<MessageService>()
+          // Logging
+          .AddLogging()
+          .AddSingleton<LogService>()
+          // Extra
+          .AddSingleton(config)
+          // Add additional services here...
+          .BuildServiceProvider();
+    }
+
+    private static IConfiguration BuildConfig()
+    {
+      try
+      {
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("config.json")
+            .Build();
+      }
+      catch (FileNotFoundException)
+      {
+        Console.WriteLine("ERROR: Could not find config.json file");
+        Environment.Exit(-1);
+        return null;
+      }
+    }
+  }
 }
---
+++ Durandal/Services/LogService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Durandal.Services
+{
+  public class LogService
+  {
+    private readonly DiscordSocketClient discord;
+    private readonly CommandService commands;
+    private readonly ILoggerFactory loggerFactory;
+    private readonly ILogger discordLogger;
+    private readonly ILogger commandLogger;
+
+    public LogService(DiscordSocketClient discord, CommandService commands, ILoggerFactory loggerFactory)
+    {
+      this.discord = discord;
+      this.commands = commands;
+
+      this.loggerFactory = ConfigureLogging(loggerFactory);
+      this.discordLogger = this.loggerFactory.CreateLogger("discord");
+      this.commandLogger = this.loggerFactory.CreateLogger("command");
+
+      this.discord.Log += LogDiscord;
+      this.commands.Log += LogCommand;
+    }
+
+    private ILoggerFactory ConfigureLogging(ILoggerFactory factory)
+    {
+      factory.AddConsole();
+      return factory;
+    }
+
+    private Task LogDiscord(LogMessage message)
+    {
+      this.discordLogger.Log(
+          LogLevelFromSeverity(message.Severity),
+          0,
+          message,
+          message.Exception,
+          (_1, _2) => message.ToString(prependTimestamp: false));
+      return Task.CompletedTask;
+    }
+
+    private Task LogCommand(LogMessage message)
+    {
+      // Return an error message for async commands
+      if (message.Exception is CommandException command)
+      {
+        // Don't risk blocking the logging task by awaiting a message send; ratelimits!?
+        var _ = command.Context.Channel.SendMessageAsync($"Error: {command.Message}");
+      }
+
+      this.commandLogger.Log(
+          LogLevelFromSeverity(message.Severity),
+          0,
+          message,
+          message.Exception,
+          (_1, _2) => message.ToString(prependTimestamp: false));
+      return Task.CompletedTask;
+    }
+
+    private static LogLevel LogLevelFromSeverity(LogSeverity severity)
+        => (LogLevel)(Math.Abs((int)severity - 5));
+  }
+}
