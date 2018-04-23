2595011bd826118e371eca2cd57b987e0b0dfd49
Recatek <recatek@hotmail.com>
2018-04-22 21:19:30 -0400

    Adding boilerplate

---
+++ Durandal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Durandal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
